a5b227293307061656454df292273b50225862e5
wanyie02513 <128675573+wanyie02513@users.noreply.github.com>
2023-05-03 23:51:35 +0800

    Update navbar.php

--- include/navbar.php
+++ include/navbar.php
@@ -15,33 +15,34 @@ session_start();
   }
 
   if ($_SESSION['accType'] == 'Admin' && isset($_SESSION['firstname'])) {
-    echo '
-      <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
-        <a class="navbar-brand ps-4" href="index.php">
-          <img src="res/logo.png" height="50" class="d-inline-block align-top" alt="logo">
-        </a>
-        <div class="collapse navbar-collapse p-2 pe-4" id="navbar">
-          <ul class="navbar-nav me-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="AddTrainingOption.php">Add Training</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="admin.php">Admin Dashboard</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="index.php?Id=GetLogOut">Log Out</a>
-            </li>
-          </ul>
-          <ul class="nav navbar-nav">
-            <li class="nav-item">
-              <span class="nav-link">'.$username.'</span>
-            </li>
-          </ul>
-        </div>
-      </nav>';
+  echo '
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+      <a class="navbar-brand ps-4" href="index.php">
+        <img src="res/logo.png" height="50" class="d-inline-block align-top" alt="logo">
+      </a>
+      <div class="collapse navbar-collapse p-2 pe-4" id="navbar">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="AddTrainingOption.php">Add Training</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="admin.php">Admin Dashboard</a>
+          </li>
+          
+        </ul>
+        <ul class="nav navbar-nav">
+          <li class="nav-item">
+            <span class="nav-link">'.$username.'</span>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?Id=GetLogOut">Log Out</a>
+          </li>
+        </ul>
+      </div>
+    </nav>';
   } 
 else{
   echo '
